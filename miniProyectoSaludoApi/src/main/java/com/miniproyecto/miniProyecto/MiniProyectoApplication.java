5ca0e67a53ea491232a34d2289243d4e3fa7a8d8
luismm2006 <luismulamarquez@gmail.com>
2025-07-28 22:19:41 +0200

    Subida de proyecto base creado con Spring initializr

---
+++ miniProyectoSaludoApi/src/main/java/com/miniproyecto/miniProyecto/MiniProyectoApplication.java
@@ -0,0 +1,13 @@
+package com.miniproyecto.miniProyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProyectoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProyectoApplication.class, args);
+	}
+
+}
---
+++ miniProyectoSaludoApi/src/test/java/com/miniproyecto/miniProyecto/MiniProyectoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miniproyecto.miniProyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProyectoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
