9ad059c5181c3036f6791fd2b4fd28a2ba1ccc40
luismm2006 <luismulamarquez@gmail.com>
2025-08-13 13:15:58 +0200

    En el Productos_Services lo que hago es básciamente la lógica de negocio, en la cual vamos a usar los datos que hemos cogido de Repo_Productos, llamamos a los metodos de dicha clase y en el findAll vamos a modificar los datos del precio, también tenemos el buscaId que su función es con el idProducto que pongamos por parametros pues devuelve de la clase Repo_Productos usando el metodo dentro de esa clase llamado buscaId, el id por parametro que hayamos asignado.

---
+++ ProyectoRepaso/springboot-id/src/main/java/com/luispractica/id/aplicacion/springboot_id/services/Productos_Services.java
@@ -0,0 +1,26 @@
+package com.luispractica.id.aplicacion.springboot_id.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import com.luispractica.id.aplicacion.springboot_id.models.Productos;
+import com.luispractica.id.aplicacion.springboot_id.repository.Repo_Productos;
+
+public class Productos_Services {
+
+    private Repo_Productos repositorio = new Repo_Productos();
+
+    public List<Productos> findAll(){
+
+        return repositorio.findAll().stream().map(p-> {
+            Double precioTotal = p.getPrecio() * 1.50d;
+            p.setPrecio(precioTotal.intValue());
+            return p;
+        }).collect(Collectors.toList());
+    }
+
+    public Productos buscaId(Long idProducto){
+
+        return repositorio.buscaId(idProducto);
+    }    
+}
