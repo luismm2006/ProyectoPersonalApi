a29d7eef024a3d749f2d5f7e1881bf4ac48c124b
luismm2006 <luismulamarquez@gmail.com>
2025-08-18 14:12:22 +0200

    Creación de la clase controller con dos metodos dentro, uno para que devuelva todos los productos y otro especificando el id, todo esto usando postman para poder visualizar su efectividad.

---
+++ ProyectoRepaso/springboot-id/src/main/java/com/luispractica/id/aplicacion/springboot_id/controllers/Product_Controller.java
@@ -0,0 +1,33 @@
+package com.luispractica.id.aplicacion.springboot_id.controllers;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.luispractica.id.aplicacion.springboot_id.models.Productos;
+import com.luispractica.id.aplicacion.springboot_id.services.Productos_Services;
+
+
+
+@RestController
+@RequestMapping("/api")
+public class Product_Controller {
+
+    private  Productos_Services servicio = new Productos_Services();
+
+    @GetMapping()
+    public List<Productos> ListaProductos(){
+        return servicio.findAll();
+    }
+
+    @GetMapping("/{idProducto}")
+    public Productos mostrar(@PathVariable long idProducto){
+
+        return servicio.buscaId(idProducto);
+
+    }
+
+}
